4fc9a225a66f53334d1e47655baa7441e37f7e57
A.K.M Nazmul Huda <nhuprim24@gmail.com>
2025-10-06 14:05:15 +0600

    facebook meta  pixel add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,13 @@ import "react-toastify/dist/ReactToastify.css";
 import dynamic from "next/dynamic";
 
 // Dynamically import the Fbpixel component with SSR disabled
-const Fbpixel = dynamic(() => import("./components/FaceBookPixel/fbpixel"));
+// const Fbpixel = dynamic(import("../app/components/FaceBookPixel/Fbpixel"));
+const Fbpixel = dynamic(
+    () => import("../app/components/FaceBookPixel/Fbpixel"),
+    {
+        ssr: false, // Disable server-side rendering for this component
+    }
+);
 
 const inter = Inter({ subsets: ["latin"] });
 
